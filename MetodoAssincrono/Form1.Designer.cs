a5c88b8d24173f2fd93df1db72f0f7b481070ba6
Alberto Monteiro <alberto.monteiro@live.com>
2014-10-17 23:43:11 -0300

    Método síncrono

--- MetodoAssincrono/Form1.Designer.cs
+++ MetodoAssincrono/Form1.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnBaixar = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pbImagem = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(15, 25);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(257, 20);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // btnBaixar
+            // 
+            this.btnBaixar.Location = new System.Drawing.Point(106, 51);
+            this.btnBaixar.Name = "btnBaixar";
+            this.btnBaixar.Size = new System.Drawing.Size(75, 23);
+            this.btnBaixar.TabIndex = 2;
+            this.btnBaixar.Text = "Baixar";
+            this.btnBaixar.UseVisualStyleBackColor = true;
+            this.btnBaixar.Click += new System.EventHandler(this.btnBaixar_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 80);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(257, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Visible = false;
+            // 
+            // pbImagem
+            // 
+            this.pbImagem.Location = new System.Drawing.Point(15, 109);
+            this.pbImagem.Name = "pbImagem";
+            this.pbImagem.Size = new System.Drawing.Size(257, 140);
+            this.pbImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImagem.TabIndex = 4;
+            this.pbImagem.TabStop = false;
+            this.pbImagem.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pbImagem);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnBaixar);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnBaixar;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pbImagem;
     }
 }
 
--- MetodoAssincrono/Form1.cs
+++ MetodoAssincrono/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,22 @@ namespace MetodoAssincrono
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnBaixar_Click(object sender, EventArgs e)
+        {
+            var webClient = new WebClient();
+
+            progressBar1.Visible = true;
+            var data = webClient.DownloadData(new Uri(txtUrl.Text));
+            progressBar1.Visible = false;
+            pbImagem.Visible = true;
+
+            using (var memoryStream = new MemoryStream(data))
+            {
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                pbImagem.Image = new Bitmap(memoryStream);
+            }
         }
     }
 }
