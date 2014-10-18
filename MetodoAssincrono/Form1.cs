af85e56b214a2b485e2a1bb8d16d35b584991981
Alberto Monteiro <alberto.monteiro@live.com>
2014-10-18 00:33:44 -0300

    Método assíncrono async e await paralelo

--- MetodoAssincrono/Form1.cs
+++ MetodoAssincrono/Form1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MetodoAssincrono
@@ -13,63 +15,85 @@ namespace MetodoAssincrono
             InitializeComponent();
         }
 
-        private void btnBaixar_Click(object sender, EventArgs e)
+        private async void btnBaixar_Click(object sender, EventArgs e)
+        {
+            var tasks = new Task<byte[]>[] { BaixaImagem1(), BaixaImagem2()/*, BaixaImagem3()*/ };
+
+            var foo = new Tuple<ProgressBar, PictureBox>[]
+            {
+                new Tuple<ProgressBar, PictureBox>(progressBar1, pbImagem),
+                new Tuple<ProgressBar, PictureBox>(progressBar3, pbImagem2),
+                new Tuple<ProgressBar, PictureBox>(progressBar2, pbImagem3)
+            };
+
+            var arrayArrayBytes = await Task.WhenAll(tasks);
+
+            for (int i = 0; i < arrayArrayBytes.Length; i++)
+            {
+                var arrayArrayByte = arrayArrayBytes[i];
+
+                foo[i].Item1.Visible = false;
+                using (var memoryStream = new MemoryStream(arrayArrayByte))
+                {
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+                    foo[i].Item2.Image = new Bitmap(memoryStream);
+                    foo[i].Item2.Visible = true;
+                }
+            }
+        }
+
+        private Task<byte[]> BaixaImagem3()
+        {
+            var webClient2 = new WebClient();
+
+            webClient2.DownloadProgressChanged += (o, args) => { progressBar2.Value = args.ProgressPercentage; };
+            progressBar2.Visible = true;
+            return webClient2.DownloadDataTaskAsync(new Uri(txtUrl2.Text));
+            /*var data3 = await webClient2.DownloadDataTaskAsync(new Uri(txtUrl2.Text));
+            progressBar2.Visible = false;
+            pbImagem2.Visible = true;
+
+            using (var memoryStream = new MemoryStream(data3))
+            {
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                pbImagem2.Image = new Bitmap(memoryStream);
+            }*/
+        }
+
+        private Task<byte[]> BaixaImagem2()
+        {
+            var webClient3 = new WebClient();
+
+            webClient3.DownloadProgressChanged += (o, args) => { progressBar3.Value = args.ProgressPercentage; };
+            progressBar3.Visible = true;
+            return webClient3.DownloadDataTaskAsync(new Uri(txtUrl3.Text));
+            /*progressBar3.Visible = false;
+            pbImagem3.Visible = true;
+
+            using (var memoryStream = new MemoryStream(data2))
+            {
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                pbImagem3.Image = new Bitmap(memoryStream);
+            }*/
+        }
+
+        private Task<byte[]> BaixaImagem1()
         {
             var webClient = new WebClient();
 
             progressBar1.Visible = true;
-            progressBar2.Visible = true;
-            progressBar3.Visible = true;
 
             webClient.DownloadProgressChanged += (o, args) => { progressBar1.Value = args.ProgressPercentage; };
-            webClient.DownloadDataCompleted += WebClientOnDownloadDataCompleted;
-            webClient.DownloadDataAsync(new Uri(txtUrl.Text));
-            
-            var webClient2 = new WebClient();
+            return webClient.DownloadDataTaskAsync(new Uri(txtUrl.Text));
+            /*
+                        progressBar1.Visible = false;
+                        pbImagem.Visible = true;
 
-            webClient2.DownloadProgressChanged += (o, args) => { progressBar2.Value = args.ProgressPercentage; };
-            webClient2.DownloadDataCompleted += WebClientOnDownloadDataCompleted2;
-            webClient2.DownloadDataAsync(new Uri(txtUrl2.Text));
-
-            var webClient3 = new WebClient();
-
-            webClient3.DownloadProgressChanged += (o, args) => { progressBar3.Value = args.ProgressPercentage; };
-            webClient3.DownloadDataCompleted += WebClientOnDownloadDataCompleted3;
-            webClient3.DownloadDataAsync(new Uri(txtUrl3.Text));
-        }
-
-        private void WebClientOnDownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
-        {
-            progressBar1.Visible = false;
-            pbImagem.Visible = true;
-
-            using (var memoryStream = new MemoryStream(e.Result))
-            {
-                memoryStream.Seek(0, SeekOrigin.Begin);
-                pbImagem.Image = new Bitmap(memoryStream);
-            }
-        }
-
-        private void WebClientOnDownloadDataCompleted2(object sender, DownloadDataCompletedEventArgs e)
-        {
-            pbImagem2.Visible = true;
-
-            using (var memoryStream = new MemoryStream(e.Result))
-            {
-                memoryStream.Seek(0, SeekOrigin.Begin);
-                pbImagem2.Image = new Bitmap(memoryStream);
-            }
-        }
-
-        private void WebClientOnDownloadDataCompleted3(object sender, DownloadDataCompletedEventArgs e)
-        {
-            pbImagem3.Visible = true;
-
-            using (var memoryStream = new MemoryStream(e.Result))
-            {
-                memoryStream.Seek(0, SeekOrigin.Begin);
-                pbImagem3.Image = new Bitmap(memoryStream);
-            }
+                        using (var memoryStream = new MemoryStream(data))
+                        {
+                            memoryStream.Seek(0, SeekOrigin.Begin);
+                            pbImagem.Image = new Bitmap(memoryStream);
+                        }*/
         }
     }
 }
